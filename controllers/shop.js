51695e1088129cb51249cc63900424d3078052c4
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-11-07 11:21:01 +0530

    update

--- controllers/shop.js
+++ controllers/shop.js
@@ -29,7 +29,7 @@ exports.getProduct = (req, res, next) => {
 	// 	.catch((err) => {
 	// 		console.log(err);
 	// 	});
-	Product.findByPk(prodId)
+	Product.findById(prodId)
 		.then((product) => {
 			res.render("shop/product-details", {
 				product: product,
