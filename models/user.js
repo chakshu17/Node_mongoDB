5685773130a75db1aacd9219340a4a558069f30a
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-11-09 10:47:06 +0530

    update

--- models/user.js
+++ models/user.js
@@ -4,9 +4,11 @@ const getDb = require("../util/database").getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-	constructor(username, email) {
+	constructor(username, email, cart) {
 		this.name = username;
 		this.email = email;
+		this.cart = cart; // {items: {}}
+		this._id = id;
 	}
 	save() {
 		const db = getDb();
@@ -18,6 +20,20 @@ class User {
 				console.log(resault);
 			})
 			.catch((err) => console.log(err));
+	}
+
+	addToCart(product) {
+		// const cartProduct = this.cart.findIndex(cp=>{
+		// 	return cp._id ===product._id;
+		// })
+		const updatedCart = { items: [{ ...product, quantity: 1 }] };
+		const db = getDb();
+		return db
+			.collection("users")
+			.updateOne(
+				{ _id: new ObjectId(this._id) },
+				{ $set: { cart: updatedCart } }
+			);
 	}
 
 	static findById(userId) {
