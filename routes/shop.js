c5f0ba6a9e3e618581776298d4fcc03fbc84a1a3
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-11-11 21:34:00 +0530

    Update

--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,9 @@
 const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
+
 const shopController = require("../controllers/shop");
 
 router.get("/", shopController.getIndex);
