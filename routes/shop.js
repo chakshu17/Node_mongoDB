c553383c7b26d9d7f9258830454837babb374210
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-11-18 12:20:10 +0530

    Update

--- routes/shop.js
+++ routes/shop.js
@@ -22,6 +22,7 @@ router.post("/create-order", shopController.postOrder);
 
 router.get("/orders", shopController.getOrders);
 
+
 // router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
