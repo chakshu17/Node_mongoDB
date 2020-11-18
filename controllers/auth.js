4292c9e51739d159a5a7f69473584d66da047674
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-11-18 13:04:04 +0530

    Cookie Parameters

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 exports.getLogin = (req, res, next) => {
-    const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[1];
+    // console.log(req.get("Cookie").split(";")[0].trim().split("=")[1] );
+	const isLoggedIn =
+		req.get("Cookie").split(";")[0].trim().split("=")[1] === "true";
 	res.render("auth/login", {
 		pageTitle: "Login",
 		path: "/login",
@@ -8,6 +10,12 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-	res.setHeader("Set-Cookie", "isloggedIn=true");
+	res.setHeader("Set-Cookie", "isloggedIn=true; ");
 	res.redirect("/");
 };
+
+
+// res.setHeader("Set-Cookie", "isloggedIn=true; Max-Age=10"); => cookie live upto 10s only i.e. no is is seconds
+// res.setHeader("Set-Cookie", "isloggedIn=true; Expires= "); => cookie live upto given date only { format can be searched }
+// res.setHeader("Set-Cookie", "isloggedIn=true; Secure"); => cookie does not allow http server to acces coookie
+// res.setHeader("Set-Cookie", "isloggedIn=true; HttpOnly"); => cookie omly allow http server to access coookie
