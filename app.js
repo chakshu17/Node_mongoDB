a76065e76a0240ce0d834623b5fffb57f8feae05
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-11-21 11:46:10 +0530

    Update

--- app.js
+++ app.js
@@ -33,29 +33,33 @@ app.use(
 		secret: "my secret",
 		resave: false,
 		saveUninitialized: false,
-		store:store,
+		store: store,
 	})
 ); // session middleware initialized
 
-// app.use((req,res,next)=>{
-// 	User.findById(req.session.user._id)
-//     .then(user => {
-//       req.user= user;
-//       next()
-//     })
-//     .catch(err => console.log(err));
-// })
-
 app.use((req, res, next) => {
-	User.findById("5fae1c0df3c5fe1ac80c4ddb")
-		.then((user) => {
-			req.user = user;
-			next();
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+	if (req.session.user) {
+		User.findById(req.session.user)
+			.then((user) => {
+				req.user = user;
+				next();
+			})
+			.catch((err) => console.log(err));
+	} else {
+		next();
+	}
 });
+
+// app.use((req, res, next) => {
+// 	User.findById("5fae1c0df3c5fe1ac80c4ddb")
+// 		.then((user) => {
+// 			req.user = user;
+// 			next();
+// 		})
+// 		.catch((err) => {
+// 			console.log(err);
+// 		});
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
